6c891ffac727557f09e3fd4fa0391f6aa786edd7
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-04-24 17:33:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kausal:kausal@cluster0.ftzlpbv.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Kusal bot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Kusal_bot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
