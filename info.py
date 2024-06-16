5867a5e83aa7fb2a59f87514ddaf798829f75845
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-06-16 07:53:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23171051'))
 API_HASH = environ.get('API_HASH', '10331d5d712364f57ffdd23417f4513c')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6780450992:AAGISeHLt1XtHl0aGVMxGV_rl_1-R2afqfs")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6780450992:AAGDqD6urWPpaSdDFnBr1EKy8kKrstJL6Ho")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 99999))
